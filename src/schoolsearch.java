bbcfd1f4cd9d8797687b87d171a7f1e7143da923
Matthew Wingerden <matthew.wingerden@gmail.com>
2022-10-01 12:57:20 -0700

    Finished Assignment.

--- src/schoolsearch.java
+++ src/schoolsearch.java
@@ -66,17 +66,18 @@ public class schoolsearch {
                     break;
                 case "Q":
                 case "Quit":
+                    System.out.println("Quitting");
                     break;
                 default:
                     System.out.println("""
                             Please print either of the following search queries:\s
-                            •S[tudent]: <lastname> [B[us]]
-                            •T[eacher]: <lastname>
-                            •B[us]: <number>
-                            •G[rade]: <number> [H[igh]|L[ow]]
-                            •A[verage]: <number>
-                            •I[nfo]
-                            •Q[uit]
+                            S[tudent]: <lastname> [B[us]]
+                            T[eacher]: <lastname>
+                            B[us]: <number>
+                            G[rade]: <number> [H[igh]|L[ow]]
+                            A[verage]: <number>
+                            I[nfo]
+                            Q[uit]
                             """);
                     break;
             }
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./run.sh <<EOF
-Grade: 1
-Student: CORKER
-Quit
-EOF
